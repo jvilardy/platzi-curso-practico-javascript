2dd13963fab55c08a39fa54712c854c38d12879a
jvilardy <ingvilardy121@gmail.com>
2022-01-20 04:00:57 -0500

    Cuarto paso del Taller#2, integrar HTML con JS

--- descuentos.js
+++ descuentos.js
@@ -12,3 +12,14 @@ function calcularPrecioConDescuento(precio, descuento) {
 //     porcentajePrecioConDescuento,
 //     precioConDescuento
 // });
+
+function calcularDescuentoPrecio() {
+    const inputPrice = document.getElementById("inputPrice").value;
+
+    const inputDisccount = document.getElementById("inputDisccount").value;
+
+    const precioConDescuento = calcularPrecioConDescuento(inputPrice, inputDisccount);
+    
+    const resultPrice = document.getElementById("resultPrice")
+    resultPrice.innerText = "El precio del producto con descuento es: $" + precioConDescuento;
+}
