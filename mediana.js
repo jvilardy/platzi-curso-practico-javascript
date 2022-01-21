9e2c3a6ed3543e3c7b145a89584c2d81bc5c6e08
jvilardy <ingvilardy121@gmail.com>
2022-01-21 01:27:18 -0500

    Segunda version de los pasos 2 y 3 del taller#3 para la Mediana

--- mediana.js
+++ mediana.js
@@ -1,21 +1,38 @@
-const lista1 = [100, 200, 500, 40000000];
+// const lista1 = [100, 200, 500, 40000000];
 
-const mitadLista1 = parseInt(lista1.length/2);
+// const mitadLista1 = parseInt(lista1.length/2);
 
-function esPar(numerito) {
-    if(numerito % 2 === 0) {
-        return true;
-    } else {
-        return false;
+// function esPar(numerito) {
+//     if(numerito % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// let mediana;
+
+// if(esPar(lista1.length)) { // numero de elementos par de lista1
+//     const elemento1 = Number(lista1[mitadLista1 - 1]);
+//     const elemento2 = Number(lista1[mitadLista1]);
+//     mediana = (elemento1 + elemento2)/2;
+// } else { // numero de elementos impar de lista1
+//     mediana = lista1[mitadLista1];
+// }
+
+function calcularMediana(lista) {
+    // Ordenando la lista
+    lista.sort(function(a,b){return a - b;});
+    
+    const mitadLista = parseInt(lista.length/2);
+    let mediana;
+
+    if(lista.length % 2 === 0) { // numero de elementos par de lista1
+        const elemento1 = Number(lista[mitadLista - 1]);
+        const elemento2 = Number(lista[mitadLista]);
+        mediana = (elemento1 + elemento2)/2;
+    } else { // numero de elementos impar de lista1
+        mediana = lista[mitadLista];
     }
-}
-
-let mediana;
-
-if(esPar(lista1.length)) { // numero de elementos par de lista1
-    const elemento1 = Number(lista1[mitadLista1 - 1]);
-    const elemento2 = Number(lista1[mitadLista1]);
-    mediana = (elemento1 + elemento2)/2;
-} else { // numero de elementos impar de lista1
-    mediana = lista1[mitadLista1];
+    return mediana;
 }
