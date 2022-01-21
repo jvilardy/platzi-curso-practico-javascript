1dd143e2b26cffb39476a6b5610b53222781e497
jvilardy <ingvilardy121@gmail.com>
2022-01-21 00:38:05 -0500

    Primera version de los pasos 2 y 3 del taller#3

--- promedio.js
+++ promedio.js
@@ -3,7 +3,19 @@ const lista1 = [100, 200, 300, 500];
 let sumalista1 = 0;
 
 for(let i = 0; i < lista1.length; i++) {
-    sumalista1 += lista1[i];  // sumalista1 = sumalista1 + lista[i]
+    sumalista1 += lista1[i];  // sumalista1 = sumalista1 + lista1[i]
 }
 
 const promedioLista1 = sumalista1/lista1.length;
+
+
+function calcularMediaAritmetica(lista) {
+    let sumalista = 0;
+
+    for(let i = 0; i < lista.length; i++) {
+        sumalista += lista[i];  // sumalista = sumalista + lista[i]
+    }
+
+    const promedioLista = sumalista/lista.length;
+    return promedioLista;
+}
